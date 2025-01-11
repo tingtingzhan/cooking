3e9a7b761695b250b70e441a0e862d424e573bf0
Tingting Zhan <tingtingzhan@gmail.com>
2025-01-11 09:26:52 -0500

    test

--- R/nutrition_noodle.R
+++ R/nutrition_noodle.R
@@ -1,7 +1,9 @@
 
-
+#' @title Starch Noodle
+#' 
+#' @export
 JayOne_sweetPotato <- function() new(
-  Class = 'nutrition',
+  Class = 'nutrition', call = match.call()[[1L]], 
   jayone = 'wide-glass-noodle-sweet-potato',
   name = 'Wide Glass Noodle, Sweet Potato\U1f360', alias = '\u5bbd\u53e3\u73bb\u7483\u7ea2\u85af\u7c89',
   usd = 9.99/1000*56, yamibuy = '25775014',
--- R/recipe_sweetPotatoNoodle.R
+++ R/recipe_sweetPotatoNoodle.R
@@ -1,17 +1,32 @@
 
 
+#' @title \linkS4class{soaked} Recipes
+#' 
+#' @name soaked
+#' @aliases soaked-class
+#' @export
+setClass(Class = 'soaked', contains = 'recipe', prototype = prototype(
+  alias_class = '\u6ce1\u53d1'
+))
+
+
+#' @rdname soaked
+#' @export
 sweetPotatoNoodle_soaked <- function() new(
-  Class = 'recipe',
+  Class = 'soaked',
   alias_flavor = '\u7ea2\u85af\u7c89', 
-  alias_class = '\u6ce1\u53d1',
-  grain = c(JayOne_sweetPotato = 581), # weighed
-  #boilingWater = (2036-898)-581 # 30min in boiling water
-  water95 = 600 # to confirm; overnight in 95C water
+  grain = c(JayOne_sweetPotato = 300), # weighed
+  boilingWater = (1933-898) - 300, # confirmed!!
+  instruction = c(
+    'prepare 1.2x boiling water',
+    'remove from heat as long as noodle softens',
+    'cool down on countertop, then chill over night'
+  )
 )
 
 
-sweetPotatoNoodle <- function() new(
-  Class = 'recipe',
-  homemade = c(sweetPotatoNoodle_soaked = 150),
-  boilingWater = 50
-)
+#sweetPotatoNoodle <- function() new(
+#  Class = 'recipe',
+#  homemade = c(sweetPotatoNoodle_soaked = 150),
+#  boilingWater = 50
+#)
