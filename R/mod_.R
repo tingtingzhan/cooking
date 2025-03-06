5cb0a0ff41f03b25bacd59f988dcdf9c86678a44
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-06 16:00:24 -0500

    test

--- R/mod_.R
+++ R/mod_.R
@@ -1,14 +1,35 @@
 
 #' @title Integer Division and Modulo with a `tol`
 #' 
-#' @param e1,e2 \link[base]{numeric} \link[base]{vector}s, see functions `base::%/%` and `base::%%`
+#' @description
+#' Integer division (see function \link[base]{%/%}) and 
+#' modulo (see function \link[base]{%%}) 
+#' at a user-specified `tol`erance.
+#' 
+#' @param e1,e2 \link[base]{numeric} \link[base]{vector}s,
+#' the shorter length will be recycled
+# see functions \link[base]{%/%} 
+# and `base::%%`
+# cannot be in @param field ?????
 #' 
 #' @param tol \link[base]{double} scalar, default `.Machine$double.eps`
 #' 
+#' @returns 
+#' Function [mod_] returns a \link[base]{numeric} scalar or \link[base]{vector} of 
+#' the integer division (similar to function \link[base]{%/%}), with an \link[base]{attributes}
+#' \describe{
+#' \item{`attr(,'e1')`}{\link[base]{numeric} scalar or \link[base]{vector}, the modulo (similar to function \link[base]{%%})}
+#' }
+#' 
 #' @examples
-#' (x = 48*2 - .Machine$double.eps * 1e3L)
+#' x0 = 48*2 - .Machine$double.eps*1e1
+#' x0 %/% 48 # good; R takes care of this
+#' x0 %% 48 # good; R takes care of this
+#' 
+#' (x = 48*2 - .Machine$double.eps*1e2)
 #' x %/% 48 # nah
 #' x %% 48 # nah
+#' 
 #' mod_(x, e2 = 48, tol = 1e-6) # yeah!
 #' @export
 mod_ <- function(e1, e2, tol = .Machine$double.eps) {
