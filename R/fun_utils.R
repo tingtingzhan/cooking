019500a198c9fcc4b01c1444fa4d135558f84a72
Tingting Zhan <tingtingzhan@gmail.com>
2026-09-13 16:22:44 -0400

    tzh

--- R/fun_utils.R
+++ R/fun_utils.R
@@ -1,5 +1,60 @@
 
 
+
+#' @title Summation by Name
+#' 
+#' @description ..
+#' 
+#' @param ... \strong{named} \link[base]{numeric} \link[base]{vector}s
+#' 
+#' @return 
+#' The function [sum_by_name()] returns a \link[base]{numeric} \link[base]{vector}.
+#' 
+#' @examples
+#' sum_by_name(1, 2)
+#' sum_by_name(1, c(a = 2))
+#' 
+#' x1 = c(a = 2, b = 3)
+#' x2 = c(b = 2, c = 1)
+#' x3 = c(a = 1, b = -2, c = -1)
+#' sum_by_name(x1, x2, x3)
+#' @export
+sum_by_name <- \(...) {
+  
+  xs <- list(...)
+  xs <- xs[lengths(xs) > 0L]
+  if (!length(xs)) return(numeric())
+  
+  ns <- lengths(xs, use.names = FALSE)
+  nms <- lapply(xs, FUN = names)
+  nms_len <- lengths(nms, use.names = FALSE)
+  if (all(ns == 1L)) {
+    ret <- sum(unlist(xs, use.names = FALSE))
+    if (all(nms_len == 0L)) return(ret)
+    if (sum(nms_len) == 1L) {
+      # beautiful! only one ingredient has name
+      names(ret) <- nms[[which(nms_len == 1L)]]
+      return(ret)
+    }
+  }
+  
+  nm <- nms |>
+    unlist(use.names = FALSE) |>
+    unique.default()
+  ret <- numeric(length = length(nm))
+  names(ret) <- nm
+  for (i in xs) ret[names(i)] <- ret[names(i)] + i
+  return(ret)
+  
+}
+
+
+
+
+
+
+
+
 # add name to len-1 vector
 .addname1 <- \(x, nm = stop('no default!'), ...) {
   
